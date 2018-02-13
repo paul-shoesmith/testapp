3cdc4eca564c02965734efd0cc37d39f4ba0ec79
Paul Shoesmith <paul.shoesmith@pilotbean.com>
2018-02-13 11:10:49 +0000

    change ip

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var route = express.Router();
 
-app.use('', route);
+app.use('/', route);
 
 app.get('/', function(req,res){
     res.send('Hello World!!');
